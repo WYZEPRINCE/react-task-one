ffc5f616ae0f1fd416436fc397dab3873f4b56e0
WYZEPRINCE <adelaniadelaja@gmail.com>
2025-06-10 23:00:06 +0100

    image path modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css'
 import ProfileCard from '../src/components/ProfileCard';
+import headshot from './assets/images/adelaniHeadshot.jpg';
 
 function App() {
   return (
     <div>
       <ProfileCard
         name="Adelani Adeyinka Adelaja"
-        picture="src\assets\images\adelaniHeadshot.jpg"
+        picture={headshot}
         bio="Creative UI/UX Designer 🎨 & Front-End web developer 👨‍💻| Millitary Personnel | ReacJs | NextJs | Crafting Human-Centered, Impactful Digital Experiences | Bridging User Needs with Business Success |Kodecamp Alumni 🎓 "
       />
     </div>
